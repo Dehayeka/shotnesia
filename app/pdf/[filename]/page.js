a34fa3454092e6d5a86590b015b9fd71695e8516
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2026-09-24 10:39:15 +0800

    edra

--- app/pdf/[filename]/page.js
+++ app/pdf/[filename]/page.js
@@ -12,6 +12,8 @@ const driveLinks = {
   'Googledrive-Prof.-Made.pdf': 'https://drive.google.com/drive/folders/1XeS055pjaR0W4zqOI7iDkqJov9PWCC8w?usp=sharing',
   'Googledrive-gia.pdf': 'https://drive.google.com/drive/folders/12xIo_oIfPXiSpMxKEGu_s9RE0l_g-ANP?usp=drive_link',
   'Googledrive-haikal.pdf': 'https://drive.google.com/drive/folders/1JfQvhM5Xx5x3iDPXfHjAlH2wxxGPyayv?usp=sharing',
+  'Googledrive-Edra.pdf': 'https://drive.google.com/drive/folders/1q5bqubgQapOqzwrjAt6xL3bGVNWaYSlo?usp=sharing',
+
 
   // Format: 'NamaFile.pdf': 'Link Google Drive',
 };
